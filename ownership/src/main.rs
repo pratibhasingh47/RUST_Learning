c5888cd3eeb38cea6d40ca633ca56f6e2cbdc1cd
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-09-17 23:47:20 +0530

    rust_update

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -64,11 +64,30 @@ fn main() {
     // 1. At any given time, you can have either one mutable reference or any no. of inmutable references.
     // 2. References must always be valid.
 
-    let mut s = String::from("String Word");
-    let word = first_word(&s);
-    let first = &s[0..6];
-    let second = &s[7..12];
-    s.clear();
+    // let mut s = String::from("String Word");
+    // let word = first_word(&s);
+    // let first = &s[0..6];
+    // let second = &s[7..12];
+    // s.clear();
+
+    let my_string = String::from("hello world");
+
+    // `first_word` works on slices of `String`s, whether partial or whole
+    let _word = first_word(&my_string[0..6]);
+    let _word = first_word(&my_string[..]);
+    // `first_word` also works on references to `String`s, which are equivalent
+    // to whole slices of `String`s
+    let _word = first_word(&my_string);
+
+    let my_string_literal = "hello world";
+
+    // `first_word` works on slices of string literals, whether partial or whole
+    let _word = first_word(&my_string_literal[0..6]);
+    let _word = first_word(&my_string_literal[..]);
+
+    // Because string literals *are* string slices already,
+    // this works too, without the slice syntax!
+    let _word = first_word(my_string_literal);
 
     let a = [1, 2, 3, 4, 5];
 
@@ -113,14 +132,27 @@ fn change(some_string: &mut String){
 //     &s
 // }
 
-fn first_word(s: &String ) -> usize{
+// fn first_word(s: &String ) -> usize{
+//     let bytes = s.as_bytes();
+
+//     for(i,&item) in bytes.iter().enumerate(){
+//         if item == b' '{
+//             return i;
+//         }
+//     }
+
+//     s.len()
+// }
+
+
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
 
-    for(i,&item) in bytes.iter().enumerate(){
-        if item == b' '{
-            return i;
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
         }
     }
 
-    s.len()
+    &s[..]
 }
