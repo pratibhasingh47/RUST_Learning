12e2c442c130d021d415984215b80b0acf35e477
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-09-17 23:43:48 +0530

    rust_update

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
 
     {
         // s is not calid here, it's not yet declared
-        let s = "hello";
+        let _s = "hello";
         // s is valid from this point forward
         // do stuff with s
     } 
@@ -15,17 +15,17 @@ fn main() {
 
 
     let x = 5;
-    let y = x; //Copy
+    let _y = x; //Copy
 
     let s1 = String::from("hello");
     // let s2 = s1; // Move (not shallow copy)
-    let s2 = s1.clone();
+    let _s2 = s1.clone();
 
     println!("{} world!",s1 );
 
     let s3 = String::from("Ownership");
     takes_ownership(s3);
-    println!("{}", s3);
+    // println!("{}", s3);
 
     let x = 47;
     makes_copy(x);
@@ -36,8 +36,49 @@ fn main() {
 
     let str1 = gives_ownership();
     let str2 = String::from("Hellooo");
-    let str3 = takes_Give_back(str2);
+    let str3 = takes_give_back(str2);
     println!("s1 = {}, s3 = {}",str1,str3);
+
+
+    let a = String::from("Heelloo");
+    let len = calculate_length(&a);
+    println!("The length of '{}' is {}. ", a , len);
+
+
+    let mut b = String::from("Change");
+    change(&mut b);
+
+    // let mut z = String::from("Error");
+    // let r1 = &mut z;
+    // let r2 = &mut z;
+    // println!("{} , {}", r1,r2);
+
+    let z = String::from("Error");
+    let r1 = &z;
+    let r2 = &z;
+    println!("{} , {}", r1,r2);
+
+    // let reference_to_nothing = dangle();
+
+    // Rules of References
+    // 1. At any given time, you can have either one mutable reference or any no. of inmutable references.
+    // 2. References must always be valid.
+
+    let mut s = String::from("String Word");
+    let word = first_word(&s);
+    let first = &s[0..6];
+    let second = &s[7..12];
+    s.clear();
+
+    let a = [1, 2, 3, 4, 5];
+
+    let slice = &a[1..3];
+
+    assert_eq!(slice, &[2, 3]);
+
+
+
+
 }
 
 fn takes_ownership(some_string: String){
@@ -53,6 +94,33 @@ fn gives_ownership() ->String{
     some_string
 }
 
-fn takes_Give_back(a_string: String) -> String{
+fn takes_give_back(a_string: String) -> String{
     a_string
+}
+
+fn calculate_length(s : &String) -> usize{
+    let length = s.len();
+    length
+}
+
+fn change(some_string: &mut String){
+    some_string.push_str(", World");     
+}
+
+// fn dangle() -> &String{
+//     let s = String::from("Hellllooo");
+
+//     &s
+// }
+
+fn first_word(s: &String ) -> usize{
+    let bytes = s.as_bytes();
+
+    for(i,&item) in bytes.iter().enumerate(){
+        if item == b' '{
+            return i;
+        }
+    }
+
+    s.len()
 }
