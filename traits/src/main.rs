f2fc231f32d100a30eac64b114f5ae88118955e3
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-09-23 00:12:09 +0530

    rust_update

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -1,3 +1,5 @@
+use aggregator::{Summary, Tweet};
+
 pub struct NewsArticle {
     pub headline: String,
     pub location: String,
@@ -22,4 +24,18 @@ impl Summary for Tweet {
     fn summarize(&self) -> String {
         format!("{}: {}", self.username, self.content)
     }
+}
+
+
+fn main() {
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
 }
