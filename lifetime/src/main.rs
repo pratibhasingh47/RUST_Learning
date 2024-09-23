860a9ece4eecd9cefbcc7788fe54a82b16e86898
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-09-24 00:04:39 +0530

    rust_update

---
+++ lifetime/src/main.rs
@@ -0,0 +1,71 @@
+// 1. Each parameter that is reference gets its own lifetime parameter.
+// 2. If there is exactly one input lifetime parameter , that lifetime is assigned to all output lifetime parameters.
+// 3. If there are multiple input lifetime parameters, but one of them is &self 0r &mut self the lifetime of self is assigned to all output lifetime parameters.
+
+
+// fn main() {
+//     let x = 5;            // ----------+-- 'b
+//                           //           |
+//     let r = &x;           // --+-- 'a  |
+//                           //   |       |
+//     println!("r: {r}");   //   |       |
+//                           // --+       |
+// }                         // ----------+
+
+use std::fmt::Display;
+
+fn longest_with_an_announcement<'a, T>(
+    x: &'a str,
+    y: &'a str,
+    ann: T,
+) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {ann}");
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+
+fn main() {
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {result}");
+
+    // let string1 = String::from("long string is long");
+    // let result;
+    // {
+    //     let string2 = String::from("xyz");
+    //     result = longest(string1.as_str(), string2.as_str());
+    // }
+    // println!("The longest string is {result}");
+
+    let s: &'static str = "I have a static lifetime.";
+
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
